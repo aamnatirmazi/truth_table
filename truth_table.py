e01f54908fda1204f7e342833c3fa14f2c518d3b
aamnatirmazi <168120014+aamnatirmazi@users.noreply.github.com>
2025-01-19 01:42:56 -0500

    Update truth_table.py

--- truth_table.py
+++ truth_table.py
@@ -3,21 +3,21 @@ truth_table_headers = ['P', 'Q', 'R', 'P -> R', 'Q -> R', '(P -> R) ^ (Q -> R)',
 
 # (P --> Q) or (Q --> R) <-> ( P and Q --> R) ?
 def truth_table(p, q, r):
-    P = 'T' if p else 'F'
-    Q = 'T' if q else 'F'
-    R = 'T' if r else 'F'
+    P = 1 if p else 0
+    Q = 1 if q else 0
+    R = 1 if r else 0
     # A: P -> R
-    A = 'T' if (not p or r) else 'F'
+    A = 1 if (not p or r) else 0
     # B: Q -> R
-    B = 'T' if (not q or r) else 'F'
+    B = 1 if (not q or r) else 0
     # C: (P -> R) ^ (Q -> R) => A and B
-    C = 'T' if A and B else 'F'
+    C = 1 if A and B else 0
     # D: P v Q  => P or Q
-    D = 'T' if (p or q) else 'F'
+    D = 1 if (p or q) else 0
     # E = P v Q -> R => not D or R
-    E = 'T' if (not D or r) else 'F'
+    E = 1 if (not D or r) else 0
     # F => (P -> R) ^ (Q -> R) <-> P v Q -> R => C == E
-    F = 'T' if C == E else 'F'
+    F = 1 if C == E else 0
 
     return P, Q, R, A, B, C, D, E, F
 grid = []
@@ -25,4 +25,5 @@ for p in [1,0]:
     for q in [1,0]:
         for r in [1,0]:
 	        grid.append(truth_table(p, q, r))
+
 print(tabulate(grid, headers = truth_table_headers, tablefmt="grid"))
